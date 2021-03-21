06cb0ba4b8b6e8dc828cee1e393985dfa5e7f59d
Sumana Sattar <sumana.sattar@hyperisland.se>
2021-03-21 17:04:48 +0100

    adding a class and constructor

--- src/App.js
+++ src/App.js
@@ -1,25 +1,38 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import './App.css';
+import {Component} from 'react';
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+      monster:[
+        {
+          name:'Khizer',
+          id:1
+        },
+        {
+          name:'Ahsan',
+          id:2
+        }, {
+          name:'Kinza',
+          id:3
+        },
+      ]
+      
+    }
+  }
+  render() {
+    return(
+      <div>
+         {this.state.monster.map(monster=>{
+           return <h1 key={monster.id}>{monster.name}</h1>
+         })}
+      </div>
+    );
+  }
+
+
 }
+  
 
 export default App;
