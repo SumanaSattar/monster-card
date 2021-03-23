bb5c583f21ec31264ce6694955dd82ed11594770
Sumana Sattar <sumana.sattar@hyperisland.se>
2021-03-23 15:54:04 +0100

    card component

--- src/Components/Card-list/Card-list.component.jsx
+++ src/Components/Card-list/Card-list.component.jsx
@@ -1,9 +1,15 @@
 import './Card-list.style.css'
+import { Card } from './../Card/Card.component'
+
 const CardList = (props) => {
+
     return(
         <div className='card-list'>
+       
           {props.monster.map(monster=>{
-           return <h1 key={monster.id}>{monster.name}</h1>
+           
+            return <Card key={monster.id} monsters={monster}/>
+      
          })}
         </div>
     )
---
+++ src/Components/Card/Card.component.jsx
@@ -0,0 +1,9 @@
+export const Card = (props) => {
+    console.log(props.monsters)
+    return ( <div>
+           
+           <h1>{props.monsters.name}</h1>
+        
+            </div> );
+}
+ 
